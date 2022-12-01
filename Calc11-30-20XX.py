c76831c142fdb43941fca782b4a1f2c80ef81417
Flames Co. Robotics <113800788+FlamesCoLTDRobotics@users.noreply.github.com>
2022-11-30 18:40:04 -0800

    [Request to upload 11.30.2022 6:39 - [FST]

---
+++ Calc11-30-20XX.py
@@ -0,0 +1,7 @@
+while True:
+    # Get user input and evaluate it
+    expr = input("Enter an arithmetic expression: ")
+    result = eval(expr)
+
+    # Print the result
+    print("The result is:", result)
