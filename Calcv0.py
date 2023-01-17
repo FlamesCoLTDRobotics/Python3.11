316c0ed7b196d5a91dbabd6877384637c9d2359c
Flames Co. Robotics <113800788+FlamesCoLTDRobotics@users.noreply.github.com>
2023-01-17 13:18:06 -0800

    [Requst 1.17.20XX] 1.0 2023
    Readme here [C] Flames LLC~ [20XX]

--- Calcv0.py
+++ Calcv0.py
@@ -1,33 +1,16 @@
-def calculator():
-  while True:
-    # Get the input from the user
-    num1 = input("Enter the first number: ")
-    operator = input("Enter the operator (+, -, *, /): ")
-    num2 = input("Enter the second number: ")
+import tkinter as tk
 
-    # Convert the input to numbers (if possible)
-    try:
-      num1 = float(num1)
-      num2 = float(num2)
-    except ValueError:
-      print("Invalid input. Please try again.")
-      continue
+root = tk.Tk() 
+root.title("Hello world!") 
+root.geometry('800x600') 
+root.resizable(width=False, height=False)   #maximize button disabled 
 
-    # Perform the requested operation
-    if operator == "+":
-      result = num1 + num2
-    elif operator == "-":
-      result = num1 - num2
-    elif operator == "*":
-      result = num1 * num2
-    elif operator == "/":
-      result = num1 / num2
-    else:
-      print("Invalid operator. Please try again.")
-      continue
+# Create a label widget of width 25 columns and 2 lines (use wraplength to limit text per line): 
+lbl = tk.Label(master=root,text="Hello world!", wraplength=25*25)   # set wrapping for long strings/words - wrap every 25 characters by default in this case  
+lbl.pack()   # pack the widgets so it displays on the main window
 
-    # Output the result
-    print("Result:", result)
+# Create a quit button
+quit_button = tk.Button(root, text="Quit", command=root.destroy)
+quit_button.pack()
 
-# Run the calculator
-calculator()
+root.mainloop()
